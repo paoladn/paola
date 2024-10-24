ba096f67f00b797804811951922d3f35ca90ec39
paola2 <166825551+paoladn@users.noreply.github.com>
2024-10-24 09:46:59 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -18,3 +18,8 @@ criaCartao(
         'O que é uma função?',
         'Uma função é um bloco de código que executa alguma tarefa'
         )
+        criaCartao(
+            'Língua Inglesa',
+            'Como se diz oi em Inglês?',
+            'Oi em inglês se diz HI (RAI)'
+            )
